b401380d52d21c51997bc43fa29264bd09955ee2
Cort Cox <cort.cox@students.snow.edu>
2024-11-08 10:25:30 -0700

    Started on auth stuff.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,39 @@
+using System.Text;
 using gameapi;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "https://auth.snowse.duckdns.org/realms/advanced-frontend",
+                ValidAudience = "cort-id",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_secret_key"))
+            };
+        });
+
 var app = builder.Build();
 app.UseCors(c => c.AllowAnyHeader()
     .AllowAnyMethod()
 .AllowAnyOrigin());
+
+app.MapGet("/authOnly", () => {
+    Console.WriteLine("In auth endpoint: ");
+    return "Hello World Authentication";
+});
+
+app.MapGet("/public", () => "Hello World Public!").AllowAnonymous();
 
 app.MapPost("/add-game", async (Game game) => 
 {
